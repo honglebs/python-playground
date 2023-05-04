bc93ca270fcbaeb055c9920cd81ad0aa5cb6b88a
Hong Le <hongle622@gmail.com>
2023-05-03 17:12:07 -0700

    added files

---
+++ mock-van/strong_password.py
@@ -0,0 +1,9 @@
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. STRING password
+#
+
+def minimumNumber(n, password):
+    # Return the minimum number of characters to make the password strong
+    return None
