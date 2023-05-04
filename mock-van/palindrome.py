a18740bb304f9b0325e3de42e9a498f6a4012852
Hong Le <hongle622@gmail.com>
2023-05-04 15:11:44 -0700

    added alternating_characters.py

---
+++ mock-van/palindrome.py
@@ -0,0 +1,12 @@
+# Palindrome
+
+# a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+# given a string of lowercase letters range ascii[a-z]
+# determine the index of a chracter that can be removed to make the string a palindrome
+
+# parameter is a string, return int (the index of which the chracter that needs to be removed)
+
+# ======= NOTES ======= #
+# aaab --> [3] 
+# baa --> [0]
+# aaa --> [-1]
