c21ab216d8f35d03970d7e06a56cde549879e82f
Hong Le <hongle622@gmail.com>
2023-05-03 20:26:52 -0700

    first added

---
+++ hackerrank-misc/first.py
@@ -0,0 +1,10 @@
+#Complete the function solveMeFirst to compute the sum of two integers
+
+#int a: first value
+#int b: second value
+
+def solve_me_first (a, b):
+    sum = a + b
+    return sum
+
+# print(solve_me_first(2, 5))
