0fc5516b41cd94511e72e4bbd24c08005e565254
Hong Le <hongle622@gmail.com>
2023-05-05 20:48:30 -0700

    struggling on repeated_string

--- mock-van/repeated_string.py
+++ mock-van/repeated_string.py
@@ -2,13 +2,54 @@
 
 # The function is expected to return a LONG_INTEGER.
 # The function accepts following parameters:
-#  1. STRING s
-#  2. LONG_INTEGER n
+#  1. STRING s (aba)
+#  2. LONG_INTEGER n (10)
 #
 
 # ====== NOTES ====== #
 
-def repeatedString(s, n):
-    # Write your code here
-    return None
+# s = aba 
+# n = 10 
+# aba.aba.aba.a
+# 1 2 3 4 5 6 7 (counting how many 'a' there are)
 
+
+
+# def repeatedString(s, n):
+
+#     # init a counter, set to 0 to count how many a's
+#     count = 0
+#     l = len(s)
+
+#     for i in range(0, l):
+#         if s[i] == "a":
+#             count += 1
+    
+#     count *= int(n/l)
+
+#     for i in range(0, n % l):
+#         if s[i] == "a":
+#             count += 1
+
+#     return count
+
+def repeatedString(s, n):
+    count = 0
+    l = len(s)
+
+    for i in range(l):
+        if s[i] == "a":
+            count += 1
+
+    count *= n // l
+
+    for i in range(n % l):
+        if s[i] == "a":
+            count += 1
+
+    return count
+
+
+print(repeatedString("aba", 10))
+print(repeatedString("noneb", 300))
+print(repeatedString("aa", 1))
