a7cba44c2ac375a038178c29e953fcb9bcac32d2
Hong Le <hongle622@gmail.com>
2023-05-12 11:03:52 -0700

    finished arith.op

---
+++ hackerrank/arithmetic_operators.py
@@ -0,0 +1,18 @@
+# TASK 
+# 1. the first line contains the sum of the two numbers
+# 2. the second line contains the difference of the two numbers (first - second)
+# 3. the third line contains the product of the two numbers
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    first_line_sum = a + b
+    second_line_diff = a - b
+    third_line_prod = a * b
+
+    print(first_line_sum)
+    print(second_line_diff)
+    print(third_line_prod)
+
