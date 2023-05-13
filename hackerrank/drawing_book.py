40983328f791b19abd40a4920097ad6b640ceb81
Hong Le <hongle622@gmail.com>
2023-05-12 21:58:50 -0700

    finished drawing_book

--- hackerrank/drawing_book.py
+++ hackerrank/drawing_book.py
@@ -6,14 +6,30 @@
 
 # return an int: the minimum numbers of pages to turn to (check front & back)
 
-def pageCount(n, p):
-    # checking our location to destination
-    current_page = 0
-    count = 0
+# def pageCount(n, p):
+#     # checking our location to destination
+#     current_page = []
+#     count = 0
     
-    for i in range(1, n + 1):
+#     # iterate through the book starting from 1 to number of pages in the book 
+#     for i in range(1, n + 1):
 
-        if current_page(i) != p:
-            count += 1
+#         # conidtion if the page we are currently on is not equal to the destination page,
+#         # we add +1 to counter (number of page turns)
+#         if current_page(i) != p:
+#             count += 1
+#     return count 
 
-print(pageCount(5))
+# print(pageCount(5, 4))
+
+def pageCount(n, p):
+    # Calculate the number of pages to turn from the beginning
+    pages_from_front = p // 2
+
+    # Calculate the number of pages to turn from the end
+    pages_from_back = (n // 2) - (p // 2)
+
+    # Return the minimum number of pages to turn
+    return min(pages_from_front, pages_from_back)
+
+print(pageCount(5, 4))
