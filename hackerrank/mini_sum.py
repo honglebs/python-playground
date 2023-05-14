858ec808754d72c5093941531ac8a98ca0827b57
Hong Le <hongle622@gmail.com>
2023-05-13 23:15:26 -0700

    files

--- hackerrank/mini_sum.py
+++ hackerrank/mini_sum.py
@@ -1,4 +1,7 @@
 # Mini Max
 
-#write down notes for this
+# write down notes for this
 # Minimum numbers to solve this
+
+# Given five positive ints, 
+# find the minimum and maximum values that can be calaculated 
