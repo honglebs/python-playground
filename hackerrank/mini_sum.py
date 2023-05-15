70e6e52673c23d995b4675b6be9b6136e4b52ab6
Hong Le <hongle622@gmail.com>
2023-05-14 21:42:47 -0700

    added some more notes

--- hackerrank/mini_sum.py
+++ hackerrank/mini_sum.py
@@ -4,4 +4,14 @@
 # Minimum numbers to solve this
 
 # Given five positive ints, 
-# find the minimum and maximum values that can be calaculated 
+# find the minimum and maximum values that can be calaculated by summing exactly
+# four of the five integers
+
+# arr = [1, 3, 5, 7, 9]
+# the minimum sum is 1 + 3 + 5 + 7 = 16
+# the maximum sun is 3 + 5 + 7 + 9 = 24
+# the function would print 16 24
+
+def miniMaxSum(arr):
+    # write some code here
+    return None
