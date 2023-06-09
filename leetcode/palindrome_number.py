820605a8eeacbbfc7f3959a5df33ef348e79853f
Hong Le <hongle622@gmail.com>
2023-06-09 13:15:38 -0700

    added notes to palindrome

---
+++ leetcode/palindrome_number.py
@@ -0,0 +1,15 @@
+# Palindrome Number
+
+# given an integer x, return true if x is a palindrome, and false otherwise
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
