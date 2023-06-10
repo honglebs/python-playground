66a6d9a21f315a5b0b11dc7dbd979c4d5e17f9c3
Hong Le <hongle622@gmail.com>
2023-06-10 07:35:36 -0700

    roman_integers started

---
+++ leetcode/roman_integer.py
@@ -0,0 +1,19 @@
+# Roman numerals are represented by seven differnt symbols: I, V, X, L, C, D, M
+
+"""
+SYMBOL     VALUE 
+I           1
+V           5
+X           10
+L           50
+C           100
+D           500
+M           1000
+"""
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
