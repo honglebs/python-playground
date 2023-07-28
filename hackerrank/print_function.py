ac7ddbfa729988f706b844f3aa50825b52bf7f60
Hong Le <hongle622@gmail.com>
2023-07-27 21:42:32 -0700

    comments

--- hackerrank/print_function.py
+++ hackerrank/print_function.py
@@ -3,7 +3,9 @@
 # print 1 2 3
 
 def printNumbers(n):
+    #create an empty list
     numbers_list = []
+    # itererate through the sequence 
     for i in range(1, n + 1):
         numbers_list.append(str(i))
     return ''.join(numbers_list)
