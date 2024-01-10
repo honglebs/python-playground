3c1aecffeb14384a4f7c93eaa18fc24baed50006
Hong Le <hongle622@gmail.com>
2024-01-10 15:41:27 -0800

    updated

---
+++ OOP/main.py
@@ -0,0 +1,18 @@
+
+class Pet():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+
+#creating the class dog with attributes and methods
+class Dog():
+    
+    #creating a method (aka a function located inside the class)
+    def bark(self):
+        print("woof woof")
+
+
+
+
