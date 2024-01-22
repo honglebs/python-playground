62757f73e25f3e2e6a6c3d7aaefda8a1a0eb01b3
hong <hongle622@gmail.com>
2024-01-22 07:33:19 -0800

    rich not working

--- OOP/main.py
+++ OOP/main.py
@@ -1,17 +1,17 @@
 
-class Pet():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Pet():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
 
 
-#creating the class dog with attributes and methods
-class Dog():
+# #creating the class dog with attributes and methods
+# class Dog():
     
-    #creating a method (aka a function located inside the class)
-    def bark(self):
-        print("woof woof")
+#     #creating a method (aka a function located inside the class)
+#     def bark(self):
+#         print("woof woof")
 
 
 
