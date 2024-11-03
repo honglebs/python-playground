46270bb08b3a8e8f81ee613abd4c8aa506276754
Hong Le <101665760+honglebs@users.noreply.github.com>
2024-11-03 13:18:03 -0800

    added microwaves attributes and functions

--- leetcode/contains_duplicates.py
+++ leetcode/contains_duplicates.py
@@ -30,11 +30,38 @@ class Microwave:
     def __init__(self, brand: str, power_rating: str) -> None:
         self.brand = brand
         self.power_rating = power_rating
+        self.turned_on: bool = False
+
+    def turn_on(self) -> None:
+        if self.turned_on:
+            print(f'Microwave ({self.brand}) is already turned on.')
+        else:
+            self.turned_on = True
+            print(f'Microwave ({self.brand}) is now turned on.')
+
+    def turn_off(self) -> None:
+        if self.turned_on:
+            self.turned_on = False
+            print(f'Microwave ({self.brand}) is now turned off.')
+        else:
+            print(f'Microwave ({self.brand}) is already turned off.')
+            
+    def run(self, seconds: int) -> None:
+        if self.turned_on:
+            print(f'Running ({self.brand}) for {seconds} seconds')
+        else:
+            print(f'A mystical force whisphers: "Turn on your microwavw first..."') 
+
 
 smeg: Microwave = Microwave('Smeg', 'B')
 
-print (smeg.brand)
-print (smeg.power_rating)
+smeg.turn_on()
+smeg.turn_on()
+
+# bosch: Microwave = Microwave('Bosch', 'C')
+# print(bosch)
+# print(bosch.brand)
+# print(bosch.power_rating)
 
 
 
