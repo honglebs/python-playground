c348cc3c0b65b085a49ff0b4c0737ae6e06d9651
Hong Le <101665760+honglebs@users.noreply.github.com>
2024-11-02 23:30:26 -0700

    leetcode contains duplicates attempt 1

--- leetcode/contains_duplicates.py
+++ leetcode/contains_duplicates.py
@@ -1,14 +1,18 @@
+"""
 # Given an integer array nums, return ture if any value appears at least twice in the array
 # and return false if every element is distinct 
 
 # Example 1:
-    
-
+    input: nums = [1, 2, 3, 1]
+    output: true
 
 # Example 2:
-
+    input: nums = [1, 2, 3, 4]
+    output: false
 
 # Example 3:
+    input: nums = [1,1,1,3,3,4,3,2,4,2]
+    output: true
 
-
+"""
 
