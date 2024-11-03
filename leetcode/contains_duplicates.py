f35df100e29e0a4d1ccc0b3f7d15e869dfb35089
Hong Le <101665760+honglebs@users.noreply.github.com>
2024-11-03 13:02:36 -0800

    test classing

--- leetcode/contains_duplicates.py
+++ leetcode/contains_duplicates.py
@@ -24,4 +24,17 @@
 
 # Approach 4: Hashmap
 
+#===================================================================
+
+class Microwave:
+    def __init__(self, brand: str, power_rating: str) -> None:
+        self.brand = brand
+        self.power_rating = power_rating
+
+smeg: Microwave = Microwave('Smeg', 'B')
+
+print (smeg.brand)
+print (smeg.power_rating)
+
+
 
