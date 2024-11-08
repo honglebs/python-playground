33cbee87b6835d23a904a969ca9c2d6a9190ad73
Hong Le <101665760+honglebs@users.noreply.github.com>
2024-11-07 22:09:15 -0800

    attempting

--- leetcode/roman_integer.py
+++ leetcode/roman_integer.py
@@ -3,6 +3,7 @@
 
 
 """
+====================================================================================
 SYMBOL     VALUE 
 I           1
 V           5
@@ -11,6 +12,21 @@ L           50
 C           100
 D           500
 M           1000
+====================================================================================
+
+so, we need to understand that there are key rules for roman numerals
+1. Additive Rule -- symboles are generally added together 
+    VI -> 5 + 1 = 6
+    XIII -> 10 + 1 + 1 + 1 = 13
+
+2. Subtractive Rule -- when a smaller numeral appears before a larger one, it is subtacted
+    IV -> 5 - 1 = 4
+    IX -> 10 -1 = 9
+    XL -> 50 - 10 = 40
+
+
+
+
 """
 
 class Solution(object):
@@ -19,3 +35,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+
+    # creating a look up table to store the roman numerals
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+
